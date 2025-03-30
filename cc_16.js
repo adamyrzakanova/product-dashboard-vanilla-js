001a107be087c205e662fd14bd01a171e9d32a24
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-03-30 17:09:13 -0400

    Task 2 - Fetch with .then()

--- cc_16.js
+++ cc_16.js
@@ -1,2 +1,15 @@
-
+// Task 2: Fetch with .then()
+function fetchProductsThen() {
+    fetch("https://www.course-api.com/javascript-store-products")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Using .then():");
+        data.forEach((product) => {
+          console.log(product.fields.name);
+        });
+      })
+      .catch((error) => {
+        handleError(error);
+      });
+  }
   
