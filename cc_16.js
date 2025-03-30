f2b696262f33ed2877f70c55797f18c20f73b7cd
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-03-30 17:11:56 -0400

    Task 3 - Fetch with async/await

--- cc_16.js
+++ cc_16.js
@@ -13,3 +13,13 @@ function fetchProductsThen() {
       });
   }
   
+  // Task 3: Fetch with async/await
+async function fetchProductsAsync() {
+    try {
+      const response = await fetch("https://www.course-api.com/javascript-store-products");
+      const products = await response.json();
+      displayProducts(products);
+    } catch (error) {
+      handleError(error);
+    }
+  }
